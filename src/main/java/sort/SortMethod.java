f1563d5465a04c845fa51518edb1893fd908db65
kadisyy <chenkunOnline@163.com>
2018-12-16 17:53:00 +0800

    quick sort

--- src/main/java/sort/SortMethod.java
+++ src/main/java/sort/SortMethod.java
@@ -2,15 +2,13 @@ package sort;
 
 import java.util.Arrays;
 
-/**
- * Created by 媛媛 on 2018/12/15.
- */
 public class SortMethod {
     private static int[] input = {2, 4, 10, 3, -9, 19, -2};
 
     public static void main(String[] args) {
 
-        BubbleSort(input);
+        //BubbleSort(input);
+        QuickSort(input,0, input.length - 1);
         show(input);
     }
 
@@ -23,6 +21,29 @@ public class SortMethod {
                     input[j + 1] = tmp;
                 }
             }
+        }
+    }
+
+    public static void QuickSort(int[] input, int start, int end) {
+        if (start <end) {
+            int stard = input[start];
+            int low = start;
+            int high = end;
+            while (low < high) {
+                while (low < high && stard <= input[high]) {
+                    high--;
+                }
+                input[low] = input[high];
+                while (low < high && input[low] <= stard) {
+                    low++;
+                }
+                input[high] = input[low];
+            }
+            input[low] = stard;
+
+            //处理右边
+            QuickSort(input, start, low);
+            QuickSort(input, low + 1, end);
         }
     }
 
